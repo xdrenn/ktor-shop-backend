2e97d1dbd162617b0c349f1e99d8eee75e57d036
drenn <burberrry3@gmail.com>
2023-08-01 16:22:31 +0300

    Routing ways

---
+++ src/main/kotlin/com/backend/routes/Router.kt
@@ -0,0 +1,34 @@
+package com.backend.routes
+
+import com.backend.controller.UserController
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Routing.users(userController: UserController) {
+    route("users") {
+        post("register") { userController.register(this.context) }
+        post("login") { userController.login(this.context) }
+    }
+}
+
+fun Route.authenticate() {
+    authenticate {
+        get("authenticate") {
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+}
+
+fun Route.getSecretInfo() {
+    authenticate {
+        get("secret") {
+            val principal = call.principal<JWTPrincipal>()
+            val userId = principal?.getClaim("userId", String::class)
+            call.respond(HttpStatusCode.OK, "Your userId is $userId")
+        }
+    }
+}
