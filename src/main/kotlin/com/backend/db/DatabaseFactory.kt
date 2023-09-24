29226e28af237a991f88c8aa04fec36c2b03d75a
Anastasia Vilmovskaya <burberrry3@gmail.com>
2023-09-24 21:59:53 +0300

    Change database url from local to Render db

--- src/main/kotlin/com/backend/db/DatabaseFactory.kt
+++ src/main/kotlin/com/backend/db/DatabaseFactory.kt
@@ -8,7 +8,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 object DatabaseFactory {
     fun init(){
         Database.connect(
-            url ="jdbc:postgresql://localhost:5432/shopdb",
+            url ="jdbc:postgresql://dpg-cjrgkm0jbais739krd8g-a.oregon-postgres.render.com:5432/postgresql_pgadmin_egxa",
             driver = "org.postgresql.Driver",
             user = System.getenv("POSTGRES_USER"),
             password = System.getenv("POSTGRES_PASSWORD"))
