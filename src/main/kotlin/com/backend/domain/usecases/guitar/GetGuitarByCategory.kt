23a98acfadab04a6ab4ac6c7bb204f1529a579d7
Anastasia Vilmovskaya <burberrry3@gmail.com>
2023-11-01 18:10:53 +0300

    Change value clothes to guitar

--- src/main/kotlin/com/backend/domain/usecases/guitar/GetGuitarByCategory.kt
+++ src/main/kotlin/com/backend/domain/usecases/guitar/GetGuitarByCategory.kt
@@ -6,8 +6,8 @@ import com.backend.utils.ServerResponse
 
 class GetGuitarByCategory(private val guitarDataSource: GuitarDataSource) {
     suspend operator fun invoke(category: String): GuitarResponse {
-        return when (val clothes = guitarDataSource.getGuitarByCategory(category)) {
-            is ServerResponse.Success -> GuitarResponse(clothes.data)
+        return when (val guitar = guitarDataSource.getGuitarByCategory(category)) {
+            is ServerResponse.Success -> GuitarResponse(guitar.data)
             is ServerResponse.Failure -> GuitarResponse(null)
         }
     }
